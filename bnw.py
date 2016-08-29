d2676242f2ef97665fa8e207034c133fa6bbc9c0
Shingo Tomioka <tomioka_shingo_15@cbms.k.u-tokyo.ac.jp>
2016-08-29 13:21:03 +0900

    Tweak

--- bnw.py
+++ bnw.py
@@ -1,23 +1,21 @@
-#! /usr/bin/env python
-
+import os
 import sys
 from ctypes import *
 
-
 class Alignment(Structure):
     _fields_ = [
-            ("seq1_aln" , c_char_p),
-            ("seq2_aln" , c_char_p),
-            ("aln_size",  c_int),
-            ("score",     c_int)
+        ("seq1_aln" , c_char_p),
+        ("seq2_aln" , c_char_p),
+        ("aln_size",  c_int),
+        ("score",     c_int)
         ]
 
-BNW = CDLL('bnw.so')
-BNW.align.argtypes = [ POINTER(c_char), c_int, POINTER(c_char), c_int, 
+dll_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib', 'bnw.so')
+bnw = CDLL(dll_path)
+
+bnw.align.argtypes = [ POINTER(c_char), c_int, POINTER(c_char), c_int, 
                    c_int, c_int, c_int, c_int, c_int ]
 
-BNW.align.restype = POINTER(Alignment)
-BNW.free_alignment.argtypes = [POINTER(Alignment)]
-
-
+bnw.align.restype = POINTER(Alignment)
+bnw.free_alignment.argtypes = [POINTER(Alignment)]
 
